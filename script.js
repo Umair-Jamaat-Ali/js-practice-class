c2626f8e9e9c3a15fc5bf5c829b10f44df12b254
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-03-22 23:44:21 +0500

    js practice

---
+++ script.js
@@ -0,0 +1,51 @@
+// js practice
+
+// data types
+
+//1 string data which used for alphabetics and its always writtien in double qotation (""), for exmple
+
+var userName = "umair jamaat Ali";
+console.log("User name is ", userName);
+
+// 2 numberic data awhich used for numeric, for example
+
+var userNumber = 04350712423;
+console.log("User number is ", userNumber);
+
+
+// 3 floating data which is used for decimal, for example
+
+var dici = 45.76
+console.log("Decimal number is ", dici);
+
+// alert is used for special message to the user
+
+alert("Thanks for visiting this page");
+console.log(alert);
+
+// math operations 
+// sum opertion
+
+var sum = 4+7;
+console.log("sum is =>", sum);
+
+// subsract operation
+
+var subs = 4-7;
+console.log("subsract is =>", subs);
+
+// multiple operation
+
+var multi = 4*7;
+console.log("multiple is =>", multi);
+
+// division operation
+
+var divi = 56/9;
+console.log("division is ", divi)
+
+// modulues operation
+
+var mod = 5%3;
+console.log("modules is ", mod);
+
